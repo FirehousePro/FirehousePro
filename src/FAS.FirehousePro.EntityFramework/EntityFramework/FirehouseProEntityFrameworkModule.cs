1ce6e378f3aac111a19555fab908a9cc6d675e84
Justin <justin@finalapproachsoftware.com>
2017-03-09 23:01:10 -0600

    Enabled code first migrations on startup

--- src/FAS.FirehousePro.EntityFramework/EntityFramework/FirehouseProEntityFrameworkModule.cs
+++ src/FAS.FirehousePro.EntityFramework/EntityFramework/FirehouseProEntityFrameworkModule.cs
@@ -2,6 +2,8 @@
 using System.Reflection;
 using Abp.Modules;
 using Abp.Zero.EntityFramework;
+using System.Data.Entity.Migrations;
+using System.Linq;
 
 namespace FAS.FirehousePro.EntityFramework
 {
@@ -12,12 +14,21 @@ namespace FAS.FirehousePro.EntityFramework
     {
         public override void PreInitialize()
         {
-            Database.SetInitializer(new CreateDatabaseIfNotExists<FirehouseProDbContext>());
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<FirehouseProDbContext, Migrations.Configuration>());
+
+
+            var configuration = new Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            if (migrator.GetPendingMigrations().Any())
+            {
+                migrator.Update();
+            }
         }
 
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
         }
+
     }
 }
