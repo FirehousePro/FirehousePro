dd0eb1ea9244a7f0bae3f3cf1e6abf9bdcbcd8e3
Justin <justin@finalapproachsoftware.com>
2017-03-15 07:36:27 -0500

    Adding base set of entities for Fire Station mgmt

---
+++ src/FAS.FirehousePro.Core/FireDepartments/FireStation.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Domain.Entities;
+using FAS.FirehousePro.Core.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAS.FirehousePro.Core.FireDepartments
+{
+    public class FireStation: Entity<int>
+    {
+        public virtual string Number { get; set; }
+        public virtual string Name { get; set; }
+        public virtual Address Address { get; set; }
+
+
+    }
+}
